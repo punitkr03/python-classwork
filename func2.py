3588f1bed5a0c9eb85ba84408223fcfd5457dd80
punitkr03 <tagpunitkumar@gmail.com>
2024-02-08 11:15:58 +0530

    funcs

---
+++ func2.py
@@ -0,0 +1,39 @@
+# def my_function(x, /):
+#     print(x)
+
+# my_function(10)
+
+
+def create_order(item_name, quantity, delivery_address):
+    print(item_name, quantity, delivery_address)
+
+# create_order('Laptop', 2, 'New York')
+    
+# def my_function(*, x):
+    # print(x)
+# 
+# my_function(x=10)
+
+def calculate_bill(bill_amount, tax_rate, tip_percentage=0.15, additional_fees=0):
+    total =  bill_amount + (bill_amount * tax_rate) + (bill_amount * tip_percentage) + additional_fees
+    print(total)
+
+# calculate_bill(100, 0.06, 0.10, 10)
+    
+def myfun (*abc):
+    for arg in abc:
+        print(arg)
+
+# myfun('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+        
+def printf(*args):
+    for arg in args:
+        print(arg, end =" ")
+
+x=2
+y=x**23
+printf("hello \n", "x = ", x, "y = ", y, "z = ", y%3 , "\n")
+
+def my_fun(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
